929f8557117ce9b56ca48359e995b52a6f5ffdd3
usman125471 <usman.125471@email.com>
2022-04-02 15:03:21 +0500

    ok

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,24 +1,22 @@
 class SearchView {
-    _parentElement = document.querySelector('.search');
-    
+  _parentElement = document.querySelector('.search');
+
   getQuery() {
-      const query= this._parentElement.querySelector('.search__field').value;
-      this._clearInput();
-      return query;
-    };
+    const query = this._parentElement.querySelector('.search__field').value;
+    this._clearInput();
+    return query;
+  }
 
-    _clearInput() {
-        this._parentElement.querySelector('.search__field').value=''
-    };
-
+  _clearInput() {
+    this._parentElement.querySelector('.search__field').value = '';
+  }
 
   addHandlerSearch(handler) {
     this._parentElement.addEventListener('submit', function (e) {
-      e.preventDefault();
+      e.preventDefault;
       handler();
     });
-    };
-
+  }
 }
 
 export default new SearchView();
